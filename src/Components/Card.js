2a02e5f678632956c159a4f8adad4d19b9ada001
Akshay pal <palakshay3091@gmail.com>
2024-04-05 10:31:41 +0530

    images

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,6 @@ import moment from "moment";
 
 
 function Card() {
-    const [weatherData, setweatherData] = useState(false)
     const { cityData } = useContext(CityContext);
     const [data, setdata] = useState('')
     const [temp, settemp] = useState('')
@@ -20,7 +19,6 @@ function Card() {
     const fetchingTime = async () => {
         try {
             const response = await axios.get("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-            console.log(response.data.datetime);
             setcurrentTime(response.data.datetime)
 
         } catch (error) {
@@ -35,7 +33,6 @@ function Card() {
             setdata(response.data);
             settemp(response.data.main);
             setweather(response.data.weather);
-            setweatherData(true)
         } catch (error) {
             console.log(error, 'this is error of fetching api in mumbai page');
         }
@@ -44,42 +41,77 @@ function Card() {
     return (
         <>
 
-            {weatherData && (
-                <div className="container mx-auto mt-5 border ">
-                    <h2 className='text-center mt-5 mb-5'>Welcome to Weather App</h2>
-                    <section className="vh-60 border rounded-pill " style={{ backgroundColor: '#f5f6f7' }}>
-                        <div className="container  p-5">
-                            <h2 className='text-center'>{data.name} Current Time {moment(currentTime).format('HH:mm:ss')} and  Weather</h2>
-                            <div className="row  d-flex justify-content-center align-items-center mt-5">
-                                <div className="col-md-10 col-lg-8 col-xl-6" >
-                                    <div className="card  bg-dark text-white">
-                                        <div className="bg-image" style={{ borderRadius: '35px' }}>
-                                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/draw1.webp"
-                                                className="card-img" alt="weather" />
-                                            <div className="mask" style={{ backgroundColor: 'rgba(190, 216, 232, .5)' }}></div>
-                                        </div>
-                                        <div className="card-img-overlay text-dark p-5">
 
-                                            <h3 className="mb-0">{data.name}, In</h3>
-                                            <hr />
-                                            {temp && (
-                                                <p className="display-2 mt-3 my-3 fs-4">Current temprature is <u><strong>{(temp.temp - 273.15).toFixed(1)}°C</strong></u></p>
-                                            )}
-                                            <p className="display-2 fs-4 mb-3">Temprature min: <strong>{(temp.feels_like - 273.15).toFixed(1)}</strong></p>
-                                            {weather && weather.length > 0 && weather.map((item, index) => (
-                                                <p className=' display-2 fs-4' key={index}>Sky Full OF : {item.main}</p>
-                                            ))}
-                                        </div>
+            <div className="container mx-auto mt-5 border ">
+                <h2 className='text-center mt-5 mb-5'>Welcome to Weather App</h2>
+                <section className="vh-60 border rounded-pill " style={{ backgroundColor: '#f5f6f7' }}>
+                    <div className="container  p-5">
+                        <h2 className='text-center'>{data.name} Current Time {moment(currentTime).format('HH:mm:ss')} and  Weather</h2>
+                        <div className="row  d-flex justify-content-center align-items-center mt-5">
+                            <div className="col-md-10 col-lg-8 col-xl-6" >
+                                <div className="card  bg-dark text-white">
+                                    <div className="bg-image" id='image' style={{ borderRadius: '35px' }}>
+                                        {weather && weather.map((item, index) => {
+                                            if (item.main === 'Smoke') {
+                                                return (
+                                                    <img src="https://c0.wallpaperflare.com/preview/992/147/619/weather-smoke-smog-pollution.jpg"
+                                                        className="card-img" alt="weather" />
+                                                );
+                                            } else if (item.main === 'Haze') {
+                                                return (
+                                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiooRr0dB2aTCCPSRm2YU_VIo_nyfs3844UQ&ss"
+                                                        className="card-img" alt="weather" style={{ objectFit: 'contain' }} />
+
+                                                );
+                                            } else if (item.main === 'Clouds') {
+                                                return (
+                                                    <img key="clouds" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBeyyPlr8CoqAU_4RfXZYJ8cLFUffa5vp1oA&s"
+                                                        className="card-img" alt="weather" style={{ objectFit: 'cover' }} />
+                                                );
+                                            } else if (item.main === 'Clear') {
+                                                return (
+                                                    <img key="clouds" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnlm9Q-8Gl8j6VrhRk0eohrzPe4JPYjCqRWQ&s"
+                                                        className="card-img" alt="weather" style={{ objectFit: 'cover' }} />
+                                                );
+                                            }
+                                            else {
+                                                return (
+                                                    <img key="default" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/draw1.webp"
+                                                        className="card-img" alt="weather" />
+                                                );
+                                            }
+
+
+                                        })}
+
+                                        <div className="mask" style={{ backgroundColor: 'rgba(190, 216, 232, .5)' }}></div>
+                                    </div>
+                                    <div className="card-img-overlay text-dark p-5 text-white">
+
+                                        <h3 className="mb-0">{data.name}, In</h3>
+                                        <hr />
+                                        {temp && (
+                                            <p className="display-2 mt-3 my-3 fs-4">Current temprature is <u><strong>{(temp.temp - 273.15).toFixed(1)}°C</strong></u></p>
+                                        )}
+                                        <p className="display-2 fs-4 mb-3">Temprature min: <strong>{(temp.feels_like - 273.15).toFixed(1)}</strong></p>
+                                        {weather && weather.length > 0 && weather.map((item, index) => (
+                                            <div key={index}>
+                                                <p className='display-2 fs-4' key={index}>Sky Full OF : {item.main}</p>
+                                                {/* {item.main === 'Smoke' && (
+                                                    console.log("Smoke")
+                                                )} */}
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
-
                             </div>
-                        </div>
-                    </section >
-                </div>
 
-            )
-            }
+                        </div>
+                    </div>
+                </section >
+            </div>
+
+
 
         </>
     )
